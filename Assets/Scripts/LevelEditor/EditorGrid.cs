20fa9e7be9cad53f5b840094eae2db77927719b7
Anthony Diggle <andiggl@microsoft.com>
2018-05-31 14:47:44 +0100

    Make level editor grid create all the tiles in the editable level.
    They are currently all the static block, but will be replaced with an
    editable block later

--- Assets/Scripts/LevelEditor/EditorGrid.cs
+++ Assets/Scripts/LevelEditor/EditorGrid.cs
@@ -7,10 +7,33 @@ public class EditorGrid : MonoBehaviour {
     [SerializeField]
     private Vector2Int tileMapSize = new Vector2Int(16, 16);
 
+    [SerializeField]
+    private GameObject editableTilePrefab;
+
 	// Use this for initialization
 	void Start () {
-		
+        ClearChildren();
+        CreateChildren();
 	}
+
+    private void CreateChildren()
+    {
+        for (var x = 0; x < tileMapSize.x; ++x )
+        {
+            for (var y = 0; y < tileMapSize.y; ++y )
+            {
+                Instantiate(editableTilePrefab, new Vector3(x, y, 0.0f), Quaternion.identity, transform);
+            }
+        }
+    }
+
+    private void ClearChildren()
+    {
+        while (transform.childCount != 0)
+        {
+            Destroy(transform.GetChild(0).gameObject);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
