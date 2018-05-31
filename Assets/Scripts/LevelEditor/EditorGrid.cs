e5ec3857fd4ca68c5b93593c23e14f8ef28078e0
Anthony Diggle <andiggl@microsoft.com>
2018-05-31 14:20:06 +0100

    Add initial pass of level editor grid script

---
+++ Assets/Scripts/LevelEditor/EditorGrid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorGrid : MonoBehaviour {
+    
+    [SerializeField]
+    private Vector2Int tileMapSize = new Vector2Int(16, 16);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
