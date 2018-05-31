3f66c62e1f806cee5cfcfadaa7b4995f515c2bdc
Anthony Diggle <andiggl@microsoft.com>
2018-05-31 15:02:36 +0100

    Add editable block script
    This will be able to be clicked on and change its state to another
    block.
    At the moment it remembers the blocks itself, might be better to store
    this in the editor class, to reduce moving parts.

---
+++ Assets/Scripts/LevelEditor/EditableBlock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditableBlock : MonoBehaviour {
+
+    [SerializeField]
+    private Sprite EmptySpace;
+    [SerializeField]
+    private Sprite Wall;
+    [SerializeField]
+    private Sprite PlayerStart;
+    [SerializeField]
+    private Sprite Coin;
+
+    private SpriteRenderer childSpriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+        childSpriteRenderer = transform.GetComponentInChildren<SpriteRenderer>();
+        childSpriteRenderer.sprite = EmptySpace;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
