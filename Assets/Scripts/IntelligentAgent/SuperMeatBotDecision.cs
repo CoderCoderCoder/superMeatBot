82f901a47b78d9a5e9184539442435200168b361
ChrisAcornley <c516792@uad.ac.uk>
2018-06-01 14:15:45 +0300

    Added modified agent and decisions scripts. Player controller kill function is public for hack reasons

--- Assets/Scripts/IntelligentAgent/SuperMeatBotDecision.cs
+++ Assets/Scripts/IntelligentAgent/SuperMeatBotDecision.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 public class SuperMeatBotDecision : MonoBehaviour, Decision
 {
-    public float rotationSpeed = 2f;
-
     public float[] Decide(
         List<float> vectorObs,
         List<Texture2D> visualObs,
@@ -20,13 +18,13 @@ public class SuperMeatBotDecision : MonoBehaviour, Decision
             // state[5] is the velocity of the ball in the x orientation. 
             // We use this number to control the Platform's z axis rotation speed, 
             // so that the Platform is tilted in the x orientation correspondingly. 
-            act.Add(vectorObs[5] * rotationSpeed);
+           // act.Add(vectorObs[5] * rotationSpeed);
 
             // state[7] is the velocity of the ball in the z orientation. 
             // We use this number to control the Platform's x axis rotation speed,  
             // so that the Platform is tilted in the z orientation correspondingly. 
-            act.Add(-vectorObs[7] * rotationSpeed);
-
+           // act.Add(-vectorObs[7] * rotationSpeed);
+			//
             return act.ToArray();
         }
 
