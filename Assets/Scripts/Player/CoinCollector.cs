cbeaa4ef3b8427dbb69741ebb6938ffc5e24a74c
Anthony Diggle <andiggl@microsoft.com>
2018-06-01 09:54:17 +0100

    Add coin and coin collection to the player

---
+++ Assets/Scripts/Player/CoinCollector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollector : MonoBehaviour {
+
+    internal int ConsumeCollectedCoins()
+    {
+        var coinsConsumed = coins;
+        ClearConsumedCoins();
+        return coinsConsumed;
+    }
+
+    internal void ClearConsumedCoins()
+    {
+        coins = 0;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Collected coin");
+        coins++;
+        
+        Destroy(other.gameObject);
+    }
+
+    private int coins = 0;
+}
