06428fe5bcaa77e815c752aef07b984311f06b83
ChrisAcornley <c516792@uad.ac.uk>
2018-06-01 11:42:08 +0300

    Added Follow player script for the out of bounds collision box

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	private GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+		this.transform.localPosition = new Vector3(player.transform.localPosition.x, -5.5f, 10f); // Holy crap, hard coded numbers are baaaaaaaaad!!!
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.localPosition = new Vector3(player.transform.localPosition.x, -5.5f, 10f);
+	}
+}
